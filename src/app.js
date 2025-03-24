79901a1b0d08aa2e3bfa57df682d85130b7549d4
chaudhuree <chaudhuree00@gmail.com>
2025-03-24 17:11:05 +0600

    added cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,15 @@ const globalErrorHandler = require('./app/middlewares/globalErrorHandler');
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://jackieo-frontend.vercel.app'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(cookieParser());
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
